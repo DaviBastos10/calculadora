98e3dcb4def72507f66e286c18b9ece3f9321fe9
Davi Silva <davi.oliveira.mengo@hotmail.com>
2021-10-21 22:01:21 -0300

    Otimização implementando laço de repetição

--- App.java
+++ App.java
@@ -6,38 +6,47 @@ public class App {
 
         int operacao, primeiroN, segundoN;
 
-        System.out.println("Digite a operação desejada: (soma = 1, sub = 2, div = 3, mult = 4");
-        operacao = entrada.nextInt();
+        do{
 
-        System.out.println("Digite o primeiro número da operação:");
-        primeiroN = entrada.nextInt();
+          System.out.println("\n~~~~~~~~~~~\nCALCULADORA\n~~~~~~~~~~~\n");
+          System.out.println("Para soma digite 1");
+          System.out.println("Para subtração digite 2");
+          System.out.println("Para divisão digite 3");
+          System.out.println("Para multiplicação digite 4");
+          System.out.println("Para sair digite 0\n~~~~~~~~~~~~\n");
+          System.out.println("Digite a operação desejada:");
 
-        System.out.println("Digite o segundo número da operação:");
-        segundoN = entrada.nextInt();
+          operacao = entrada.nextInt();
 
-        if (operacao == 1){
-      System.out.println("O resultado de sua operação é:"+ (primeiroN + segundoN));
-        }
+          if (operacao != 0){
+                       
+            
+          System.out.println("Digite o primeiro número da operação:");
+            primeiroN = entrada.nextInt();
+
+          System.out.println("Digite o segundo número da operação:");
+           segundoN = entrada.nextInt();
+
         
-        if (operacao == 2){
-          System.out.println("O resultado de sua operação é:"+ (primeiroN - segundoN));
+        
+          if (operacao == 1){
+            System.out.println("O resultado de sua operação é:"+ (primeiroN + segundoN));
+        }
+          if (operacao == 2){
+            System.out.println("O resultado de sua operação é:"+ (primeiroN - segundoN));
         }
        
-        if (operacao == 3){
-          System.out.println("O resultado de sua operação é:"+ (primeiroN / segundoN));
-        }
-
-        if (operacao == 4){
-          System.out.println("O resultado de sua operação é:"+ (primeiroN * segundoN));
-        }
-        else{
-          System.out.println("O código de operação é inválido! ;)");
+          if (operacao == 3){
+           System.out.println("O resultado de sua operação é:"+ (primeiroN / segundoN));
         }
 
-       
-       
-       
+          if (operacao == 4){
+            System.out.println("O resultado de sua operação é:"+ (primeiroN * segundoN));
+        }
+          
+      }
+    } while (operacao != 0); 
+                     
      entrada.close();
     }
-}
-//modificação
+  }
